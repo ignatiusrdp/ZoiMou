8690cb3b8c7eb660e408d3042b4bc012e45afd91
ignatius rahardi <ignatiusrahardi@gmail.com>
2020-12-18 14:37:54 +0700

    cleaning code and add string value

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -8,10 +8,7 @@ extern "C" JNIEXPORT jstring JNICALL
 Java_id_ac_ui_cs_mobileprogramming_ignatiusrahardi_zoimou_services_GetWeatherService_getTime(
         JNIEnv* env,
         jobject /* this */) {
-    // current date/time based on current system
     time_t now = time(0);
-
-// Convert now to tm struct for local timezone
     tm* localtm = localtime(&now);
 
 
